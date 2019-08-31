f27ae84183a076bced41357472a8ced46d87f688
drai06 <you@example.com>
2019-08-31 11:06:32 -0500

    tests

--- cypress/integration/empresariales.spect.js
+++ cypress/integration/empresariales.spect.js
@@ -1,20 +1,40 @@
 describe('My first e2e testing', function(){
-    it ('Check website', function(){
+    /* it ('Check website', function(){
         //Arrange
-        /* visit some place */
+        // -- visit some place
         cy.visit('https://example.cypress.io');
 
         //Act
-        /* Search an element & click */
+        // --Search an element & click
         cy.contains('type').click()
 
         //Assert
-        /* Cambiar url */
+        // - Cambiar url
         cy.url()
             .should('include', '/commands/action');
         //cy.get('.action-email')
         cy.get('#email1')
             .type('sidney.aguirrec@gmail.com')
             .should('have.value', 'sidney.aguirrec@gmail.com')
+    }) */
+
+    it('MELI Test', function() {
+        cy.visit('http://localhost:4200/')
+        cy.contains('Estefany')
+        cy.get('input')
+            .type('iphone 8 plus 64gb')
+            .should('have.value', 'iphone 8 plus 64gb')
+        cy.get('button')
+            .click()
+        cy.contains('iPhone')
+
+        //Search a different item
+        cy.get('input')
+            .clear() //limpiar el campo input
+            .type('Nikon D750')
+            .should('have.value', 'Nikon D750')
+        cy.get('button')
+            .click()
+        cy.contains('Nikon D750')
     })
 })
